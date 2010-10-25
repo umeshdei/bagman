3b65e7744fb009bc2841385df7818b3c1f3507d8
grzmiel <grzmiel@gmail.com>
2010-10-25 21:46:33 +0000

    Bagman v 0.0

---
+++ bagman/src/Calculation.cpp
@@ -0,0 +1,18 @@
+/*
+ * Calculation.cpp
+ *
+ *  Created on: 24-10-2010
+ *      Author: qba
+ */
+
+#include "Calculation.h"
+
+Calculation::Calculation() {
+}
+
+Calculation::~Calculation() {
+}
+
+vector<int> *Calculation::solve(Generate *pgenData) {
+	return NULL;
+}
---
+++ bagman/src/Generate.h
@@ -0,0 +1,38 @@
+/*
+ * generate.h
+ *
+ *  Created on: 24-10-2010
+ *      Author: qba
+ */
+
+#ifndef GENERATE_H_
+#define GENERATE_H_
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+
+#include "Point.h"
+
+#define MAX_VALUE 100
+
+using namespace std;
+
+class Generate {
+public:
+	Generate(int piNumberOfCities);
+	Generate(string psFileName);
+	virtual ~Generate();
+	int getDistance(int piFirstCityNumber, int piSecondCityNumber);
+	int calculateWholeDistance(vector <int> *pvCitiesSequence);
+	void saveTable(string psFileName);
+	int getNumberOfCities();
+private:
+	void generateDistances();
+	int _iNumberOfCities;
+	int **_iaDistanceBetweenCities;
+};
+
+#endif /* GENERATE_H_ */
