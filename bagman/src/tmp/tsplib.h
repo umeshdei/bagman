364176de8871ef9fdc9b9792182930c65ed87a60
grzmiel <grzmiel@gmail.com>
2010-11-13 16:46:48 +0000

    solution

--- bagman/src/tmp/tsplib.h
+++ bagman/src/tmp/tsplib.h
@@ -70,26 +70,6 @@ typedef enum {
 	SPECIAL
 } tsplib_edge_weight_type;
 
-struct {
-	char *str;
-	size_t len;
-	tsplib_edge_weight_type type;
-} tsplib_edge_weight_type_strings[] = {
-	{ "EXPLICIT", 8, EXPLICIT},
-	{ "EUC_2D", 6, EUC_2D},
-	{ "EUC_3D", 6, EUC_3D},
-	{ "MAX_2D", 6, MAX_2D},
-	{ "MAX_3D", 6, MAX_3D},
-	{ "MAN_2D", 6, MAN_2D},
-	{ "MAN_3D", 6, MAN_3D},
-	{ "CEIL_2D", 7, CEIL_2D},
-	{ "GEOM", 4, GEOM},
-	{ "ATT", 3, ATT},
-	{ "XRAY_1", 6, XRAY_1},
-	{ "XRAY_2", 6, XRAY_2},
-	{ "SPECIAL", 7, SPECIAL},
-	{ NULL, 0, UNKNOWN}
-};
 
 typedef enum {
 	FUNCTION,
